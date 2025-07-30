ce5f23746f0941fceaf81516d5922d8e7f4497e3
Ganímedes <nicososatrigo@gmail.com>
2025-07-30 03:19:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//variables
+/*
+0=comun
+1=raro
+2=extremadamente raro
+3=legendario
+4=mitico
+*/
+let rareza=[
+    2,  //pablo
+    3   //leonardo
+];
+
+let nfts = document.querySelectorAll(".nft");
+
+for (let i = 0; i < nfts.length; i++) {
+    let nft = nfts[i];
+
+    // Buscamos el span dentro de .nft-desc que representa la rareza
+    let spanRareza = nft.querySelector(".rareza + span");
+
+    // Según el valor del array, le agregamos la clase correspondiente
+    if (rareza[i] === 3) {
+        spanRareza.classList.add("legen");
+        spanRareza.textContent = "Legendario";
+    } else if (rareza[i] === 2) {
+        spanRareza.classList.add("extraro");
+        spanRareza.textContent = "Extremadamente Raro";
+    } else if (rareza[i] === 1) {
+        spanRareza.classList.add("raro");
+        spanRareza.textContent = "Raro";
+    } else if (rareza[i] === 0) {
+        spanRareza.classList.add("comun");
+        spanRareza.textContent = "Común";
+    } else if (rareza[i] === 4) {
+        spanRareza.classList.add("mitico");
+        spanRareza.textContent = "Absolutamente Mítico";
+    }
+}
